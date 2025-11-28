b79c5ffbb4cb9ff56a0e177dc1b1fa8f8913175d
Mohit kumawat <mohitkumawat221004@gmail.com>
2025-11-28 22:35:06 +0530

    restore sothing important

--- backend/backend/config/db.js
+++ backend/backend/config/db.js
@@ -1,13 +1,9 @@
-const mongoose = require("mongoose");
-
+import mongoose from "mongoose";
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected!");
-  } catch (err) {
-    console.error("MongoDB Error:", err);
-    process.exit(1);
-  }
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB Connected");
+    }
+    catch (err) { console.error(err); process.exit(1); }
 };
-
-module.exports = connectDB;
+export default connectDB;
