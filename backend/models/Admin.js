1dbbf7f7618eaa8ee493a8326a144e8bcfaafbcf
Mohit kumawat <mohitkumawat221004@gmail.com>
2025-11-28 23:22:19 +0530

    delete uneccesary

--- backend/models/Admin.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const AdminSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String, // simple string password as you asked
-});
-
-module.exports = mongoose.model("Admin", AdminSchema);
--- backend/models/Client.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ClientSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String, // simple string password
-});
-
-module.exports = mongoose.model("Client", ClientSchema);
