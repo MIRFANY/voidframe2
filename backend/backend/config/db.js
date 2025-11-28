4d41f7887ad198825c198c6aeb324e3b2524dca9
Mohit kumawat <mohitkumawat221004@gmail.com>
2025-11-28 22:17:32 +0530

    .env added to db.js

--- backend/backend/config/db.js
+++ backend/backend/config/db.js
@@ -1,6 +1,13 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
+
 const connectDB = async () => {
-    try { await mongoose.connect(process.env.MONGO_URI); console.log("MongoDB Connected"); }
-    catch (err) { console.error(err); process.exit(1); }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected!");
+  } catch (err) {
+    console.error("MongoDB Error:", err);
+    process.exit(1);
+  }
 };
-export default connectDB;
+
+module.exports = connectDB;
